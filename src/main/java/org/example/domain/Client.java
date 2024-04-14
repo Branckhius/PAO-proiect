d61218929580c19aaba43a2c373794d3aee22d80
Ion Dragos Constantin <dragos29_boss@yahoo.com>
2024-04-14 16:30:06 +0300

    Etapa1

---
+++ src/main/java/org/example/domain/Client.java
@@ -0,0 +1,44 @@
+package org.example.domain;
+
+public class Client extends Persoana {
+    private String adresa;
+    private String email;
+    private String telefon;
+
+    public Client(int id, String nume, String adresa, String email, String telefon) {
+        super(id, nume);
+        this.adresa = adresa;
+        this.email = email;
+        this.telefon = telefon;
+    }
+    void setAdresa(String Adresa){
+        this.adresa=Adresa;
+    }
+    void setEmail(String Email){
+        this.email=Email;
+    }
+    void setTelefon(String Telefon){
+        this.telefon=Telefon;
+    }
+    public String getAdresa() {
+        return adresa;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", nume='" + nume + '\'' +
+                ", adresa='" + adresa + '\'' +
+                ", email='" + email + '\'' +
+                ", telefon='" + telefon + '\'' +
+                '}';
+    }
+}
